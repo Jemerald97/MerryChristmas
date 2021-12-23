0f39fb58e7c08a21f2684bea1c90d1a0ddeb66dd
jeje-afun <jungwon@a-fun.co.kr>
2021-12-24 01:34:20 +0900

    Add image 및 selection 완성 전

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,66 @@ import Snowfall from 'react-snowfall'
 import Selection from './Selection'
 import { ReactComponent as MerryRabbit } from '../assets/svgs/MerryRabbit.svg'
 import { ReactComponent as InitialTree } from '../assets/svgs/Tree.svg'
-let i = 0
+import OrangeSock from '../assets/pngs/orangeSock.png'
+import PurpleSock from '../assets/pngs/purpleSock.png'
+import BlueStick from '../assets/pngs/blueStick.png'
+import RedStick from '../assets/pngs/redStick.png'
+import GoldStar from '../assets/pngs/goldStar.png'
+import SilverStar from '../assets/pngs/silverStar.png'
+import BlueSnow from '../assets/pngs/blueSnow.png'
+import PinkSnow from '../assets/pngs/pinkSnow.png'
+import PresentBox from '../assets/pngs/presentBox.png'
+import Heart from '../assets/pngs/heart.png'
+
+let i = 5
+const CHECKLIST = [
+  {
+    time: '11:00AM',
+    left: '잠을 조금 더 자고싶다..!',
+    right: '아이스링크장',
+    leftImg: OrangeSock,
+    rightImg: PurpleSock,
+    top: 10,
+    left: -10,
+  },
+  {
+    time: '1:00PM',
+    left: '장어 덮밥',
+    right: '고기 짬뽕',
+    leftImg: Heart,
+    rightImg: Heart,
+  },
+  {
+    time: '3:00PM',
+    left: '의자 편한 카페',
+    right: '디저트 존마탱 카페',
+    leftImg: BlueStick,
+    rightImg: RedStick,
+  },
+  {
+    time: '5:00AM',
+    left: '별마당 도서관 트리',
+    right: '명동 신세계 트리',
+    leftImg: PresentBox,
+    rightImg: PresentBox,
+  },
+  {
+    time: '7:00AM',
+    left: '샤브샤브',
+    right: '홈디너파티',
+    leftImg: SilverStar,
+    rightImg: GoldStar,
+  },
+]
+const MESSAGE = [
+  '안뇽 병군삐❤️',
+  `나 물어볼 거 있어ㅎㅎ`,
+  '크리스마스 때 계획을',
+  '오빠가 완성해주지 않을래?',
+  '바보야!!',
+  '둘 중에 하고 싶은거 나무에 걸어줘!!',
+]
+
 const shake = keyframes`
     0%{
         transform: rotate(-10deg);
@@ -48,22 +107,25 @@ const HeaderTitle = styled.div`
   font-size: 1.5rem;
   font-family: 'Gamja Flower', cursive;
 `
-
+const Caution = styled.div`
+  font-size: 5px;
+  animation: ${(props) => props.fadeIn} 5s infinite;
+`
 const Tree = styled(InitialTree)`
+  position: absolute;
+  left: 0px;
   animation: ${fadeIn} 2s linear;
 `
-
-const MESSAGE = [
-  '안뇽 병군삐❤️',
-  `나 물어볼 거 있어ㅎㅎ`,
-  '크리스마스 때 계획을',
-  '오빠가 완성해주지 않을래?',
-  '바보야!!',
-  '둘 중에 하고 싶은거 나무에 걸어줘!!',
-]
+const Img = styled.img`
+  position: relative;
+  width: 50px;
+  top: ${(props) => props.top}px;
+  left: ${(props) => props.left}px;
+`
 
 const Header = () => {
   const [message, setMessage] = useState(MESSAGE[0])
+
   const onChangeMessage = () => {
     i === 5 ? (i = 0) : (i = i + 1)
     console.log(i)
@@ -73,9 +135,26 @@ const Header = () => {
     <HeaderContainer>
       <Snowfall snowflakeCount={80} />
       {i !== 5 && <ShakingRabbit onClick={onChangeMessage} />}
-      {i === 5 && <Tree />}
       <HeaderTitle>{message}</HeaderTitle>
-      {i === 5 && <Selection fadeIn={fadeIn} />}
+      {i === 5 && (
+        <>
+          <Tree />
+          <Img src={OrangeSock} />
+          <Img src={BlueStick} />
+          <Img src={Heart} />
+          <Img src={SilverStar} />
+          <Img src={PresentBox} />
+          <Img src={BlueSnow} />
+        </>
+      )}
+      {i === 5 && (
+        <>
+          <Selection fadeIn={fadeIn} />
+          <Caution fadeIn={fadeIn}>
+            {'주의: 약간의 답정너가 포함되어 있는 점 양해 부탁드립니다.'}
+          </Caution>
+        </>
+      )}
     </HeaderContainer>
   )
 }
--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,54 +1,91 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import styled from 'styled-components'
-import A from '../assets/svgs/heart.svg'
-
+import OrangeSock from '../assets/pngs/orangeSock.png'
+import PurpleSock from '../assets/pngs/purpleSock.png'
+import BlueStick from '../assets/pngs/blueStick.png'
+import RedStick from '../assets/pngs/redStick.png'
+import GoldStar from '../assets/pngs/goldStar.png'
+import SilverStar from '../assets/pngs/silverStar.png'
+import PresentBox from '../assets/pngs/presentBox.png'
+import Heart from '../assets/pngs/heart.png'
+let i = 0
 const CHECKLIST = [
   {
     time: '11:00AM',
-    left: '아직 문 안 연 카페',
+    left: '잠을 조금 더 자고싶다..!',
     right: '아이스링크장',
+    leftImg: OrangeSock,
+    rightImg: PurpleSock,
   },
   {
     time: '1:00PM',
     left: '장어 덮밥',
     right: '고기 짬뽕',
+    leftImg: Heart,
+    rightImg: Heart,
   },
   {
     time: '3:00PM',
     left: '의자 편한 카페',
     right: '디저트 존마탱 카페',
+    leftImg: BlueStick,
+    rightImg: RedStick,
   },
   {
     time: '5:00AM',
     left: '별마당 도서관 트리',
     right: '명동 신세계 트리',
+    leftImg: PresentBox,
+    rightImg: PresentBox,
   },
   {
     time: '7:00AM',
     left: '샤브샤브',
     right: '홈디너파티',
+    leftImg: SilverStar,
+    rightImg: GoldStar,
   },
+]
+const MESSAGE = [
+  '안뇽 병군삐❤️',
+  `나 물어볼 거 있어ㅎㅎ`,
+  '크리스마스 때 계획을',
+  '오빠가 완성해주지 않을래?',
+  '바보야!!',
+  '둘 중에 하고 싶은거 나무에 걸어줘!!',
 ]
 
 const CheckList = styled.div`
   font-family: 'Gamja Flower', cursive;
 `
 
-const Caution = styled.div`
-  font-size: 5px;
-  animation: ${(props) => props.fadeIn} 5s infinite;
+const SelectionContainer = styled.div`
+  position: relative;
+  top: 400px;
+  flex-direction: row;
+`
+const Img = styled.img`
+  width: 50px;
 `
 
-const Selection = ({ fadeIn }) => {
+const Selection = () => {
   const [selection, setSelection] = useState(CHECKLIST[0])
+  const [deco, setDeco] = useState(false)
+  const onSelect = () => {
+    i = i + 1
+    setSelection(CHECKLIST[i])
+  }
   return (
-    <>
-      <CheckList>{selection.left}</CheckList>
-      <CheckList>{selection.right}</CheckList>
-      <Caution fadeIn={fadeIn}>
-        {'주의: 약간의 답정너가 포함되어 있는 점 양해 부탁드립니다.'}
-      </Caution>
-    </>
+    <div>
+      <SelectionContainer>
+        <Img src={selection.leftImg} id="1" onClick={onSelect} />
+        <CheckList>{selection.left}</CheckList>
+      </SelectionContainer>
+      <SelectionContainer>
+        <Img src={selection.rightImg} id="2" onClick={onSelect} />
+        <CheckList>{selection.right}</CheckList>
+      </SelectionContainer>
+    </div>
   )
 }
 
